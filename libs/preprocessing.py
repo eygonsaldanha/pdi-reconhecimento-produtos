19f87d47897abe84561260aec56046974cabdf2f
Gbrl454 <gabriel100matos@gmail.com>
2025-10-23 20:10:09 -0300

    Cria banco de dados

--- libs/preprocessing.py
+++ libs/preprocessing.py
@@ -1,11 +1,13 @@
 import cv2
-import numpy as np
+
 
 def converter_para_cinza(imagem):
     return cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
+
 def aplicar_filtro_gaussiano(imagem, tamanho_kernel=(5, 5)):
     return cv2.GaussianBlur(imagem, tamanho_kernel, 0)
+
 
 def detectar_bordas_canny(imagem, limiar_baixo=100, limiar_alto=200):
     return cv2.Canny(imagem, limiar_baixo, limiar_alto)
