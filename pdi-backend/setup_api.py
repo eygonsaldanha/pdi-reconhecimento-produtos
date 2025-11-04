b4f6e69e7907af98e3b0e7a4ac9f85b9d34e77b6
Ice <gabriel.matos@unifor.br>
2025-11-04 20:14:47 -0300

    exemplo teste de processos

--- pdi-backend/setup_api.py
+++ pdi-backend/setup_api.py
@@ -8,7 +8,7 @@ import cv2
 
 def insert_data_in_product(full_path_file, id_product):
     path_data = generate_hash(full_path_file)
-    id_data = select_data('SELECT COUNT(*) FROM DATA')['count'].iloc[0] + 1
+    id_data = select_data('SELECT COUNT(*) FROM DATA')['count'].iloc[0] + 2
     
     insert_data('data', [{'id_data': id_data, 'path_data': path_data, 'tp_data': 'IMG', 'dt_inclusion': datetime.datetime.now()}])
     insert_data('product_data', [{'id_product': id_product, 'id_data': id_data}])
@@ -18,11 +18,11 @@ def insert_data_in_product(full_path_file, id_product):
     
     upload_img(img, f'image/{extension}', key=path_data)
 
-path = '../dataset'
+path = 'dataset'
 for name in os.listdir(path):
     full_path = os.path.join(path, name)
     if os.path.isdir(full_path):
-        id_product = select_data("SELECT COUNT(*) FROM PRODUCT")['count'].iloc[0] + 1
+        id_product = select_data("SELECT COUNT(*) FROM PRODUCT")['count'].iloc[0] + 2
         insert_data('product', [{'id_product': id_product, 'nm_product': name, 'vl_product': round(5 + (random.random() * 15), 2)}])
         for full_path_file in os.listdir(full_path):
             full_path_file = os.path.join(full_path, full_path_file)
